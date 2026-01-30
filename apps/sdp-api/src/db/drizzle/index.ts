4336e1c7973660343ef31cd6e3cd1875ff16d7ea
guibibeau <g.bibeaulaviolette@gmail.com>
2026-01-30 13:18:38 +0700

    Limit Drizzle setup to D1 only

--- apps/sdp-api/src/db/drizzle/index.ts
+++ apps/sdp-api/src/db/drizzle/index.ts
@@ -1,4 +1,2 @@
 export { createD1Drizzle } from "./d1";
-export { createPgDrizzle } from "./pg";
 export type { D1DrizzleDb } from "./d1";
-export type { PgDrizzleDb } from "./pg";
--- apps/sdp-api/src/db/repositories/base.ts
+++ apps/sdp-api/src/db/repositories/base.ts
@@ -1,3 +1,3 @@
-import type { D1DrizzleDb, PgDrizzleDb } from "../drizzle";
+import type { D1DrizzleDb } from "../drizzle";
 
-export type DrizzleDbClient = D1DrizzleDb | PgDrizzleDb;
+export type DrizzleDbClient = D1DrizzleDb;
